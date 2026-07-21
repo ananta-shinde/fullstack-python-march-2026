f69b9bd1378a240bb96913db6818b7a2e999fd2e
aShinde <ananta.shinde@honeywellcom>
2026-07-21 18:04:44 +0530

    21/07/2026

--- frontend/src/components/ImageGallary/ImageGallary.jsx
+++ frontend/src/components/ImageGallary/ImageGallary.jsx
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import "./imagegallary.css"
+import ImageSlider from "./ImageSlider";
 
 const ImmageGallary = () => {
 
@@ -7,17 +8,17 @@ const ImmageGallary = () => {
 
     const images = [
         "https://greendroprecycling.com/wp-content/uploads/2017/04/GreenDrop_Station_Aluminum_Can_1.jpg",
-        "https://png.pngtree.com/png-vector/20191120/ourmid/pngtree-cosmetic-serum-pipette-bottle-package-png-image_2009766.jpg"
+        "https://png.pngtree.com/png-vector/20191120/ourmid/pngtree-cosmetic-serum-pipette-bottle-package-png-image_2009766.jpg",
+        "https://png.pngtree.com/png-vector/20191120/ourmid/pngtree-cosmetic-serum-pipette-bottle-package-png-image_2009766.jpg",
+        "https://png.pngtree.com/png-vector/20191120/ourmid/pngtree-cosmetic-serum-pipette-bottle-package-png-image_2009766.jpg",
+        "https://png.pngtree.com/png-vector/20191120/ourmid/pngtree-cosmetic-serum-pipette-bottle-package-png-image_2009766.jpg",
+        "https://png.pngtree.com/png-vector/20191120/ourmid/pngtree-cosmetic-serum-pipette-bottle-package-png-image_2009766.jpg",
     ]
 
     return ( 
         <div className="image-gallary-container">
             <img className="preview-image" src={images[currentIndex]} alt="" />
-            <div className="thumbnail-list">
-                {images.map((img,index)=>(
-                    <img className="thumbnail" src={img} alt="" onClick={()=>setCurrentIndex(index)} />
-                ))}
-            </div>
+            <ImageSlider images={images} setCurrentIndex={setCurrentIndex}/>
         </div>
      );
 }
